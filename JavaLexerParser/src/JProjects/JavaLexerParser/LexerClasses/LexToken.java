e2c8eda920352d6b7cdd32bd26e3b8c4bc81f7e1
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-07 19:13:24 -0400

    Added LexToken Class

---
+++ JavaLexerParser/src/JProjects/JavaLexerParser/LexerClasses/LexToken.java
@@ -0,0 +1,9 @@
+package JProjects.JavaLexerParser.LexerClasses;
+public class LexToken {
+    String Contents;
+    String TokenName;
+    LexToken(String x,String y) {
+        Contents=x;
+        TokenName=y;
+    }
+}
