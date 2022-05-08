ce4d5f4d4f7b0f70562e154efc3b64396885083d
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-07 20:31:24 -0400

    Added other parameter

--- JavaLexerParser/src/JProjects/JavaLexerParser/LexerClasses/Lexer.java
+++ JavaLexerParser/src/JProjects/JavaLexerParser/LexerClasses/Lexer.java
@@ -4,14 +4,18 @@ import java.util.regex.*;
 public class Lexer {
     private HashMap<String,Pattern> tokensTypeHashMap=new HashMap<String,Pattern>();
     private ArrayList<LexToken> TokenizedOutput;
-    public int AddTokenType(String x,String y) {
+    public int AddTokenType(String x,String y,boolean Other) {
         Pattern Regex;
         try {
            Regex=Pattern.compile(y); 
         } catch (PatternSyntaxException e) {
             return 1;
         }
-        tokensTypeHashMap.put(x, Regex);
+        if (Other) {
+            tokensTypeHashMap.put("Other", Regex);
+        } else {
+            tokensTypeHashMap.put(x,Regex)
+        }
         return 0;
     }
     public int RemoveTokenType(String x) {
@@ -23,6 +27,7 @@ public class Lexer {
         }
     }
     public int Lex(String string) {
+        string StringLength=0
         return 0;
     }
 }
